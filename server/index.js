3575bb043fb69b5bf427254a67e3f251c9bc9af3
khoi-lam <khoilam.work@gmail.com>
2026-02-04 10:51:02 +0700

    init backend && connect firebase admin

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const firebaseAdmin = require('firebase-admin');
+
+const serviceAccountKey = require('./skipli-code-challenge-key.json');
+
+//init admin 
+firebaseAdmin.initializeApp({
+
+  credential: firebaseAdmin.credential.cert(serviceAccountKey)
+
+});
+
+//get instance
+const database = firebaseAdmin.firestore();
+
+console.log("firebase admin connected successfully");
